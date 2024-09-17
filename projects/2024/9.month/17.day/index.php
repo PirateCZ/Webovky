40ad8edbc498dd9c8fc3913c931bb589901a5ed0
PirateCZ <fschod@seznam.cz>
2024-09-17 12:05:47 +0200

    first phph project

---
+++ projects/2024/9.month/17.day/index.php
@@ -0,0 +1,18 @@
+<?php
+include("index.html");
+echo "HAHA<br>";
+echo "HAHA<br>";
+echo "HAHA<br>";
+echo "HAHA<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "NO<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$hello = "zhis is a var";
+echo $hello;
+?>
