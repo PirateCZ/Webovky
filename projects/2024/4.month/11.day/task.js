6f29b931b5b29bd80ec499357d064975edcef145
PirateCZ <fschod@seznam.cz>
2024-11-14 11:46:37 +0100

    removed few projects to lower html density

--- projects/2024/4.month/11.day/task.js
+++
@@ -1,8 +0,0 @@
-class task{
-    constructor(taskId, description, priority, status){
-       this.taskId = taskId
-       this.description = description
-       this.priority = priority
-       this.status = status
-    }
-}
